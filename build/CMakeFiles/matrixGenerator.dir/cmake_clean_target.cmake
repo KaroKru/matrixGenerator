6e2184202816711b7ad120a49d8f315469c5ce80
KaroKru <92943379+KaroKru@users.noreply.github.com>
2024-07-31 16:52:16 +0200

    added config file

--- build/CMakeFiles/matrixGenerator.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libmatrixGenerator.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/workspace/matrixGenerator
-# Build directory: C:/workspace/matrixGenerator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MatrixGeneratorTests "C:/workspace/matrixGenerator/build/matrixGeneratorTests.exe" "--gtest_output=xml:report.xml")
-set_tests_properties(MatrixGeneratorTests PROPERTIES  _BACKTRACE_TRIPLES "C:/workspace/matrixGenerator/CMakeLists.txt;48;add_test;C:/workspace/matrixGenerator/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
